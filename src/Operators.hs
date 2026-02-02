6143a00f09068481c86f68d04ec1dc8a056e88ec
mgurga <motiurga@gmail.com>
2026-02-02 11:45:44 -0800

    implement move blank up operation

--- src/Operators.hs
+++ src/Operators.hs
@@ -1,12 +1,43 @@
-module Operators (operators) where
+module Operators (
+    operators,
+    move_blank_up,
+    move_blank_down,
+    move_blank_left,
+    move_blank_right,
+    blank_index
+) where
 
-import State
+import State ( State (..), is_solved )
 
 -- stubs for all operations that can be applied to a State
 
+-- helper to recursivly find index of 0 in puzzle board (Int list)
+blank_index_rec :: [Int] -> Int -> Int
+blank_index_rec [] acc = acc
+blank_index_rec (x:tl) acc =
+    if x /= 0 then
+        blank_index_rec tl (acc+1)
+    else
+        acc
+
+blank_index :: [Int] -> Int
+blank_index b = blank_index_rec b 0
+
 move_blank_up :: (State) -> (State)
-move_blank_up start =
-    start
+move_blank_up start = do
+    let bi = blank_index (board start)
+    let new_bi = bi - (size start)
+    if new_bi < 0 then
+      start -- return input state if blank tile would move into invalid position
+    else do
+      let new_board = zipWith (\i element -> 
+              if i == bi then 
+                  (board start)!!new_bi -- if at blank tile index replace the 0 with above number
+              else if i == new_bi then 
+                  (board start)!!bi     -- if at above number replace the value with blank tile (0)
+              else element) [0..] (board start)
+      let basic_state = State { size=(size start), board=new_board, solved=False }
+      State { size=(size start), board=new_board, solved=is_solved(basic_state) }
 
 move_blank_down :: (State) -> (State)
 move_blank_down start =
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,5 @@
-import State (State (board), solved_state)
+import State (State (..), solved_state)
+import Operators (move_blank_up, move_blank_down, move_blank_left, move_blank_right, blank_index)
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -6,7 +7,7 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [stateTests]
+tests = testGroup "Tests" [stateTests, operatorTests]
 
 stateTests :: TestTree
 stateTests = testGroup "State tests"
@@ -25,4 +26,33 @@ stateTests = testGroup "State tests"
                                     14, 13, 12, 11, 10,
                                     9,  8,  7,  6,  5,
                                     4,  3,  2,  1,  0]
+  ]
+
+operatorTests :: TestTree
+operatorTests = testGroup "Operator tests"
+  [ testCase "Blank index check 1" $
+      (blank_index (board (solved_state 3))) @?= 8
+
+  , testCase "Blank index check 2" $
+      (blank_index ([8, 7, 3, 6, 5, 0, 2, 1])) @?= 5
+
+  , testCase "Blank index check 3" $
+      (blank_index ([8, 7, 0, 6, 5, 3, 2, 1])) @?= 2
+
+  , testCase "Move blank up test 1" $
+      (board (move_blank_up (solved_state 3))) @?= [8, 7, 6,
+                                                    5, 4, 0,
+                                                    2, 1, 3]
+
+    -- should not change board b/c blank tile is in top row
+  , testCase "Move blank up test 2" $ do
+      let test_state = State {
+        solved=False, size=3, board=[
+            8, 0, 6,
+            5, 4, 3,
+            2, 1, 7
+        ]
+      }
+      (board (move_blank_up (test_state))) @?= 
+        [8, 0, 6, 5, 4, 3, 2, 1, 7]
   ]
