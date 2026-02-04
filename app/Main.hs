0524a0f8a8f71ae5709f636bf9a31b370d7bbbd6
mgurga <motiurga@gmail.com>
2026-02-03 17:25:32 -0800

    generate solvable puzzels using random shuffle

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,14 @@
 module Main (main) where
 
-import System.Random.Shuffle (shuffleM)
-import State (State(..), solved_state)
-import Lib (get_children)
+import State (solved_state)
+import Lib (get_children, shuffle_state)
 import Data.Foldable (for_)
 
 main :: IO ()
 main = do
-    -- generate random State (might not be solvable)
+    -- generate random State that is solvable
     let goal_state = solved_state 3
-    random_board <- shuffleM (board goal_state)
-    let random_state = State {size=3, board=random_board, solved=False}
+    random_state <- shuffle_state goal_state 20
 
     -- print state and all its children
     putStrLn ("random state: " ++ (show random_state))
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,8 @@
-module Lib (get_children) where
+module Lib (get_children, shuffle_state) where
 
 import State (State)
 import Operators (operators)
+import System.Random.Shuffle (shuffleM)
 
 apply_operators :: State -> [State -> State] -> [State] -> [State]
 apply_operators parent (op:tl) children =
@@ -12,3 +13,14 @@ apply_operators _ [] children =
 get_children :: (State) -> [State]
 get_children given =
     (apply_operators given operators [])
+
+-- given a starting state and number n, randomly apply operations n times
+-- this process ensures the state is solvable
+shuffle_state :: State -> Int -> IO State
+shuffle_state initial shuffles_left = do
+    random_operators <- shuffleM operators
+    let random_operator = random_operators!!0
+    if shuffles_left == 1 then
+        return (random_operator initial)
+    else do
+        shuffle_state (random_operator initial) (shuffles_left - 1)
