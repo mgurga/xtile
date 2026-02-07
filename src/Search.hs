fcbd40f80e1d0ec00374f5dc40da618cdc49b8e2
mgurga <motiurga@gmail.com>
2026-02-06 19:04:36 -0800

    implement basic uniform cost search

--- src/Search.hs
+++ src/Search.hs
@@ -1,7 +1,6 @@
 module Search (search, SearchOptions(..), SearchResults(..)) where
 
 import State
-import StateTree (StateTree, state_tree_from_state)
 import Queue
 import Data.Set (Set)
 import Lib (get_children)
@@ -14,12 +13,18 @@ data SearchOptions = SearchOptions {
     heuristic_func :: (State -> Int)
 }
 
+-- helper data type to use with Queue
+data StateResults = StateResults {
+    state :: State,
+    results :: SearchResults
+}
+
 -- internal search data b/w recursive search calls
 data SearchInternal = SearchInternal {
-    tree :: StateTree,
     seen_states :: Set State,
-    queue :: Queue State,
-    interim_results :: SearchResults
+    queue :: Queue StateResults,
+    best_results :: SearchResults,
+    done :: Bool
 }
 
 -- these parameters are returned after the search finishes
@@ -29,24 +34,78 @@ data SearchResults = SearchResults {
     largest_queue :: Int
 } deriving (Show, Eq)
 
+-- find best results given 2 search states
+results_compare :: SearchResults -> SearchResults -> SearchResults
+results_compare a b = do
+    -- compare depths, expaneded nodes, then largest_queue
+    if (depth a) == (depth b) then do
+        if (expanded_nodes a) == (expanded_nodes b) then do
+            if (largest_queue a) < (largest_queue b) then a else b
+        else if (expanded_nodes a) < (expanded_nodes b) then a else b
+    else if (depth a) < (depth b) then a else b
+
+-- start search by passing options to astar_search
 search :: SearchOptions -> SearchResults
 search options = do
     let res = astar_search
             options
             SearchInternal {
-                tree=state_tree_from_state (start_state options),
                 seen_states=(Set.singleton (start_state options)),
-                queue=(Queue.fromList (get_children (start_state options))),
-                interim_results=SearchResults {depth=0, expanded_nodes=0, largest_queue=0}
+                queue=
+                    (Queue.singleton -- create queue with only the start node and 0 depth
+                        StateResults {
+                            state=(start_state options),
+                            results=SearchResults {depth=0, expanded_nodes=0, largest_queue=0}}),
+                best_results=
+                    SearchResults {depth=maxBound, expanded_nodes=maxBound, largest_queue=maxBound},
+                done=False
             }
-    (interim_results res)
+    (best_results res)
 
+-- this function recursivly calls itself with SearchInternal which holds the queue and final result
 astar_search :: SearchOptions -> SearchInternal -> SearchInternal
 astar_search options internal = do
-    SearchInternal {
-        tree=(tree internal),
-        seen_states=(seen_states internal),
-        queue=(queue internal),
-        interim_results=(interim_results internal)
-    }
+    let front = Queue.dequeue (queue internal) -- get first state in queue
+    let out = case front of
+            Nothing -> (
+                -- queue is empty return "failure"
+                SearchInternal {
+                    seen_states=(seen_states internal),
+                    queue=(queue internal),
+                    best_results=(best_results internal),
+                    done=True
+                })
+            Just (val, tl) -> do
+                if (state val) == (end_state options) then
+                    -- first item in queue is goal state, mark as done and return
+                    SearchInternal {
+                        seen_states=(seen_states internal),
+                        queue=(queue internal),
+                        best_results=(results val),
+                        done=True
+                    }
+                else do
+                    let children = (get_children (state val))
+                    -- TODO: sort children by depth + heuristic
+                    let newstats = 
+                         SearchResults {
+                             depth = (depth (results val)) + 1,
+                             expanded_nodes = (expanded_nodes (results val)) + (length children),
+                             largest_queue = (length (queue internal))
+                         }
+                    -- add all children nodes to queue with new results
+                    let newqueue =
+                         foldr (\child acc -> 
+                            (Queue.enqueue StateResults {state=child, results=newstats} acc)
+                         ) (tl) children
 
+                    -- call astar_search with new queue and updated best_results
+                    astar_search
+                        options
+                        SearchInternal {
+                            seen_states=(seen_states internal),
+                            queue=newqueue,
+                            done=False,
+                            best_results=(results_compare newstats (results val))
+                        }
+    out
